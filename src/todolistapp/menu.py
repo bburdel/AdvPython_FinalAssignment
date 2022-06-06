c2279d8685cc24b819c7e9ec15e05f43010da886
bburdel <94432814+bburdel@users.noreply.github.com>
2022-06-05 21:26:06 -0700

    Stopping for the evening. Wrote TODOs as reminders, need lots of clean up and editing.

--- src/todolistapp/menu.py
+++ src/todolistapp/menu.py
@@ -24,22 +24,22 @@ def add_task():
     Task status defaults to 'Not Started'
     """
     try:
-        task_name = input('Task name: ')
-        task_details = input('Task description: ')
-        start_date = input('Task start date: ')
-        due_date = input('Task due date: ')
+        print('Let\'s gather details about your task.')
+        task_name = input('Enter a Task name (e.g., Resolve script error): ')
+        task_details = input('Task description (e.g., Use pysnooper on pesky function): ')
+        start_date = input('Task start date (e.g., MM/DD/YYYY): ')
+        due_date = input('Task due date (e.g., MM/DD/YYYY): ')
         if not t.add_task(task_name, task_details, start_date, due_date):
             print("An error occurred while trying to add a new task.")
         else:
             print("Task added successfully.")
     except ValueError:
-        print("The date cannot resemble the chaos you entered.")
+        print("The date(s) cannot resemble the chaos you entered.")
 
 
 def update_task():
     """
-
-    :return:
+    Modifies the details of an existing task
     """
     # TODO perhaps give options for which to alter
     task_name = input('Task name: ')
@@ -64,6 +64,9 @@ def delete_task():
 
 
 def complete_task():
+    """
+    Marks a task in the database as completed
+    """
     task_name = input('Task name: ')
     if not t.complete_task(task_name):
         print("An error occurred while trying to mark task, 'Complete'.")
@@ -72,6 +75,9 @@ def complete_task():
 
 
 def list_task_options():
+    """
+    Basic menu for a user to select various list format options
+    """
     # List all tasks sorted by task number
     # List all tasks sorted by priority
     # List all open tasks sorted by due date
@@ -87,13 +93,14 @@ def list_task_options():
 
     while True:
         user_choice = input("""
-                                a: Tasks by Task ID
-                                b: Tasks by Priority
-                                c: Open Tasks by Date
-                                d: Closed Tasks in Date Range
-                                e: Overdue Tasks
-                                f: Return to Main Menu
-                                Please enter your choice: """)
+        Select a List view of your Tasks:
+                            a) Tasks by Task ID
+                            b) Tasks by Priority
+                            c) Open Tasks by Date
+                            d) Closed Tasks in Date Range
+                            e) Overdue Tasks
+                            f) Return to Main Menu
+                            Please enter your choice: """)
         if user_choice.lower() in submenu_options:
             submenu_options[user_choice.lower()]()
         elif user_choice.lower() == 'f':
@@ -104,15 +111,18 @@ def list_task_options():
 
 def task_id_list():
     print("""
-                See Task ID List Sorted in:
-                1 -- Ascending order
-                2 -- Descending order
+                        See Task ID List Sorted in:
+                        1 -- Ascending order
+                        2 -- Descending order
     """)
-    choice = input('Choice: ').strip()
-    if not tl.task_list_id_sort(choice):
-        print("An error occurred while trying to compile this list.")
-    else:
-        print('List generated!')
+    try:
+        choice = input('Enter 1 or 2: ').strip()
+        if not tl.task_list_id_sort(choice):
+            print("An error occurred while trying to compile this list.")
+        else:
+            print('List generated!')
+    except Exception as e:
+        logger.info(e)
 
 
 def priority_task_list():
@@ -123,20 +133,40 @@ def priority_task_list():
         print('Priority list generated!')
 
 
+# List all open tasks sorted by due date
 def due_date_list():
+    print("Open tasks sorted by due date")
+    print("""
+                        1 -- Oldest to Newest
+                        2 -- Newest to Oldest
+    """)
+    # try:
+    #     choice = input('Enter 1 or 2: ').strip()
+    #     if not tl.task_list_open_sort(choice):
+    #         print("An error occurred while trying to compile this list.")
+    #     else:
+    #         print('List generated!')
+    # except Exception as e:
+    #     logger.info(e)
     pass
 
 
 def closed_tasks_date_query():
-    pass
+    try:
+        date_1 = input("Enter first (oldest) date in range: ")
+        date_2 = input("Enter second (newest) date in range: ")
+        tl.task_list_completed_sort(date_1, date_2)
+    except Exception as e:
+        logger.info(e)
 
 
 def overdue_tasks():
+    print("Here are the Overdue Tasks:")
     pass
 
 
 def generate_task_report():
-    print("Here is the current state of your AccompList Tasks:")
+    print("Here is the record of All your AccompList Tasks:")
     return tl.database_report()
 
 
@@ -155,8 +185,8 @@ if __name__ == '__main__':
     menu_options = {
         'A': add_task,
         'B': update_task,
-        'C': delete_task,
-        'D': complete_task,
+        'C': complete_task,
+        'D': delete_task,
         'E': list_task_options,
         'F': generate_task_report,
         'Q': quit_program
@@ -164,11 +194,13 @@ if __name__ == '__main__':
 
     # print menu of options to user
     while True:
+        print("Welcome to AccompList!"
+              "\nLet us curate a List of tasks you will to Accomplish. ")
         user_selection = input("""
                                 A: Add a task
                                 B: Update a task
-                                C: Delete a task
-                                D: Complete a task
+                                C: Complete a task
+                                D: Delete a task
                                 E: List tasks
                                 F: Generate a task report
                                 Q: Quit
--- src/todolistapp/task.py
+++ src/todolistapp/task.py
@@ -1,14 +1,20 @@
-'''
-task related functions
-'''
+"""
+Title: task.py
+Description: Code containing classes that organize task details like dates and lists,
+as well as manipulate those task in a SQLite3 database.
+Author:
+Version: Python 3.10
+"""
+
+# pylint: disable=R1710
 
 import datetime as d
 from datetime import datetime
 from loguru import logger
 import peewee as pw
-import task_model as tm
 from tabulate import tabulate
 import pysnooper
+import task_model as tm
 
 
 class DateHelper:
@@ -26,7 +32,7 @@ class DateHelper:
             print("Invalid date. Cannot assign a task to the past.")
             return False
         else:
-            print("\tDate accepted.")
+            logger.info("\tDate accepted.")
             return True
 
     @staticmethod
@@ -70,13 +76,14 @@ class Task:
             return priority
 
     @staticmethod
-    @pysnooper.snoop(depth=1)
+    # @pysnooper.snoop(depth=1)
     def add_task(task_name, task_description, start_date, due_date):
         # create a task_id automatically
         task_id = datetime.now().strftime('%Y%m%d%H%M%S')
 
         try:
-            new_task = tm.Tasks.create(task_id=task_id, task_name=task_name, task_details=task_description,
+            new_task = tm.Tasks.create(task_id=task_id, task_name=task_name,
+                                       task_details=task_description,
                                        task_start_date=start_date, task_due_date=due_date,
                                        task_priority=Task.get_priority(start_date, due_date))
             new_task.save()
@@ -136,12 +143,6 @@ class TaskLists:
     Curates functions relating to organizing tasks into lists
     """
 
-    # List all tasks sorted by task number
-    # List all tasks sorted by priority
-    # List all open tasks sorted by due date
-    # List all closed tasks between specified dates
-    # List all overdue tasks
-
     @staticmethod
     def print_any_list_choice(query_results):
         formatted_list = []
@@ -157,10 +158,12 @@ class TaskLists:
             formatted_content = [task_id, task_name, task_details, task_start_date,
                                  task_due_date, task_complete_date, task_priority, task_status]
             formatted_list.append(formatted_content)
-        print("----------------------------------------------------------------------------------")
-        print(tabulate(formatted_list, headers=['Task ID', 'Name', 'Details', 'Start Date', 'Due Date',
-                                                'Completed On', 'Priority', 'Status'], tablefmt='github'))
-        print("----------------------------------------------------------------------------------")
+        print("-"*100)
+        print(tabulate(formatted_list, headers=['Task ID', 'Name', 'Details',
+                                                'Start Date', 'Due Date',
+                                                'Completed On', 'Priority', 'Status'],
+                       tablefmt='github'))
+        print("-"*100)
 
     @staticmethod
     def database_report():
@@ -176,13 +179,16 @@ class TaskLists:
             task_priority = row['task_priority']
             task_status = row['task_status']
             formatted_content = [task_id, task_name, task_details, task_start_date,
-                                 task_due_date, task_priority, task_status]
+                                 task_due_date, task_complete_date, task_priority, task_status]
             formatted_list.append(formatted_content)
-        print("----------------------------------------------------------------------------------")
-        print(tabulate(formatted_list, headers=['Task ID', 'Name', 'Details', 'Start Date', 'Due Date',
-                                                'Completed On', 'Priority', 'Status'], tablefmt='github'))
-        print("----------------------------------------------------------------------------------")
+        print("-"*100)
+        print(tabulate(formatted_list, headers=['Task ID', 'Name', 'Details',
+                                                'Start Date', 'Due Date',
+                                                'Completed On', 'Priority', 'Status'],
+                       tablefmt='github'))
+        print("-"*100)
 
+    # List all tasks sorted by task number
     @staticmethod
     def task_list_id_sort(choice):
         # filtered_options = ['In Progress', 'Complete']
@@ -190,16 +196,16 @@ class TaskLists:
         try:
             if choice.strip() == '1':
                 print('Task IDs in Ascending order:')
-                query = tm.Tasks.select().where(tm.Tasks.task_status.in_(['In Progress', 'Complete']))\
+                query = tm.Tasks.select().where(tm.Tasks.task_status != 'Deleted') \
                     .order_by(+tm.Tasks.task_id)
                 for result in query.dicts():
                     list_of_dicts.append(result)
-                print(list_of_dicts)
+                # print(list_of_dicts)
                 TaskLists.print_any_list_choice(list_of_dicts)
                 # string formatting instead?
             elif choice.strip() == '2':
                 print('Task IDs in Descending order:')
-                query = tm.Tasks.select().where(tm.Tasks.task_status.in_(['In Progress', 'Complete']))\
+                query = tm.Tasks.select().where(tm.Tasks.task_status != 'Deleted') \
                     .order_by(-tm.Tasks.task_id)
                 query_dict = query.dicts()
                 TaskLists.print_any_list_choice(query_dict)
@@ -209,12 +215,54 @@ class TaskLists:
             print("Something went wrong, please try again.")
             logger.info(e)
 
+    # List all tasks sorted by priority
     @staticmethod
     def task_list_priority_sort():
+        """
+        Uses peewee to query SQLite database for all (non-deleted) tasks
+        """
         try:
-            query = tm.Tasks.select().where(tm.Tasks.task_status != 'Deleted')\
+            query = tm.Tasks.select().where(tm.Tasks.task_status != 'Deleted') \
                 .order_by(tm.Tasks.task_due_date, tm.Tasks.task_priority)
             for row in query:
-                print(f"{row.task_priority}, {row.task_due_date}, {row.task_name}, {row.task_details} ")
+                print(f"{row.task_priority}, {row.task_due_date}, "
+                      f"{row.task_name}, {row.task_details} ")
         except Exception as e:
             logger.info(e)
+
+    # List all open tasks sorted by due date
+    @staticmethod
+    def task_list_open_sort():
+        """
+        Uses peewee to query SQLite database for all (non-deleted tasks) and sorts them by date.
+        """
+        # TODO present options to sort from oldest to newest, newest to oldest
+        pass
+
+    # List all closed tasks between specified dates
+    @staticmethod
+    def task_list_completed_sort(date_1, date_2):
+        """
+        Uses peewee to query SQLite database for completed tasks that fall within a given date range
+        """
+        # Validate dates
+        formatted_date1 = DateHelper.date_conversion(date_1)
+        formatted_date2 = DateHelper.date_conversion(date_2)
+        # TODO Finish query to include the date range
+        query = tm.Tasks.select().where(tm.Tasks.task_status == 'Completed')
+        pass
+
+    # List all overdue tasks
+    @staticmethod
+    def task_list_overdue_sort():
+        """
+        Uses peewee to query SQLite database for overdue tasks
+        """
+        list_of_dicts = []
+        today = datetime.now()
+        query = tm.Tasks.select().where(tm.Tasks.task_due_date < today)\
+            .order_by(+tm.Tasks.task_due_date)
+        for result in query.dicts():
+            list_of_dicts.append(result)
+        TaskLists.print_any_list_choice(list_of_dicts)
+
