fba211f2e5ba1223a861e4686ceac2db83c3880d
bburdel <94432814+bburdel@users.noreply.github.com>
2022-06-12 13:11:23 -0700

    Pushing the work so far today

--- src/todolistapp/accomplist_cli.py
+++ src/todolistapp/accomplist_cli.py
@@ -1,5 +1,5 @@
 """new file to experiment with creating a CLI app with typer, rich, and sqlite3
-created with the help of the youtube video: https://youtu.be/ynd67UwG_cI"""
+created with the help of the YouTube video: https://youtu.be/ynd67UwG_cI"""
 
 import typer
 from rich.console import Console
@@ -15,32 +15,118 @@ app = typer.Typer()
 
 @app.command(short_help="Adds a task to the task database")
 def create(task_name: str, task_description: str, start_date: str, due_date: str):
+    """
+    Placeholder text
+    :param task_name:
+    :param task_description:
+    :param start_date:
+    :param due_date:
+    :return:
+    """
     t.add_task(task_name, task_description, start_date, due_date)
     typer.secho(f"Adding, '{task_name}'to the list.", fg=typer.colors.BRIGHT_CYAN)
 
 
-@app.command(short_help="Updates a task in the task database")
-def update(task_name: str, task_details: str, start_date: str, due_date: str):
-    t.update_task(task_name, task_details, start_date, due_date)
-    typer.secho(f"Updating {task_name}...")
+@app.command(help="Mark a task, 'Completed'")
+def complete(task_name: str = typer.Option(default=None,
+                                           prompt="Enter exact Task Name to mark, 'Complete'",
+                                           prompt_required=True)):
+    """
+    Placeholder text
+    :param task_name:
+    :return:
+    """
+    t.complete_task(task_name)
 
 
 @app.command(help="Marks as task in the database as, 'Deleted'")
 def delete(task_name: str = typer.Option("Task name to delete: ",
                                          help="Exact name of the task to be deleted")):
+    """
+    Placeholder text
+    :param task_name:
+    :return:
+    """
     t.delete_task(task_name)
     typer.echo(f"Deleting {task_name}...")
 
 
+@app.command(short_help="Updates a task in the task database")
+def update(task_name: str, task_details: str, start_date: str, due_date: str):
+    """
+    Placeholder text
+    :param task_name:
+    :param task_details:
+    :param start_date:
+    :param due_date:
+    :return:
+    """
+    t.update_task(task_name, task_details, start_date, due_date)
+    typer.secho(f"Updating {task_name}...")
+
+
 @app.command(short_help="Creates a table of all existing database contents")
 def print_all_tasks():
+    """
+    Placeholder text
+    :return:
+    """
     tl.database_report()
     # return True
 
 
-@app.command(short_help="List of the tasks sorted by Task ID")
-def list_ids(choice: str = typer.Option(default="1", prompt="Enter 1 for Ascending or 2 for Descending: ", prompt_required=True)):
+@app.command(short_help="List tasks sorted by Task ID")
+def list_ids(choice: str = typer.Option(default=None,
+                                        prompt="Enter 1 for Ascending or 2 for Descending",
+                                        prompt_required=True)):
+    """
+    Placeholder text
+    :param choice:
+    :return:
+    """
     tl.task_list_id_sort(choice)
+
+
+@app.command(short_help="List tasks by priority")
+def list_priority():
+    """
+    Placeholder text
+    :return:
+    """
+    tl.task_list_priority_sort()
+
+
+@app.command(short_help="List incomplete tasks")
+def list_open(choice: str =
+              typer.Option(default=None,
+                           prompt="Enter 1 for Oldest to Newest or 2 for Newest to Oldest",
+                           prompt_required=True)):
+    """
+    Placeholder text
+    :param choice:
+    :return:
+    """
+    tl.task_list_open_sort(choice)
+
+
+@app.command(short_help="List all completed tasks between a date range")
+def list_between(date_1: str, date_2: str):
+    """
+    Placeholder text
+    :param date_1:
+    :param date_2:
+    :return:
+    """
+    tl.task_list_completed_sort(date_1, date_2)
+
+
+@app.command(short_help="List all overdue tasks.")
+def list_overdue():
+    """
+    Placeholder text
+    :return:
+    """
+    tl.task_list_overdue_sort()
 
 
 if __name__ == '__main__':
--- src/todolistapp/task.py
+++ src/todolistapp/task.py
@@ -8,8 +8,6 @@ Version: Python 3.10
 
 # pylint: disable=R1710
 
-# import typer
-# from rich.console import Console
 
 import datetime as d
 from datetime import datetime
@@ -18,11 +16,6 @@ import peewee as pw
 from tabulate import tabulate
 import pysnooper
 import task_model as tm
-
-
-# console = Console()
-#
-# app = typer.Typer()
 
 
 class DateHelper:
@@ -85,7 +78,6 @@ class Task:
 
     @staticmethod
     # @pysnooper.snoop(depth=1)
-    # @app.command(short_help="Adds a task to the task database.")
     def add_task(task_name, task_description, start_date, due_date):
         # create a task_id automatically
         task_id = datetime.now().strftime('%Y%m%d%H%M%S')
@@ -102,7 +94,6 @@ class Task:
             return False
 
     @staticmethod
-    # @app.command(short_help="Adds a task to the task database.")
     def update_task(task_name, task_details, start_date, due_date):
         try:
             row = tm.Tasks.get(tm.Tasks.task_name == task_name)
@@ -116,7 +107,6 @@ class Task:
             logger.info("")
 
     @staticmethod
-    # @app.command(short_help="Adds a task to the task database.")
     def delete_task(task_name):
         """
         Marks the status column of a task as 'Deleted.'
@@ -133,7 +123,6 @@ class Task:
 
     @staticmethod
     # @pysnooper.snoop()
-    # @app.command(short_help="Adds a task to the task database.")
     def complete_task(task_name):
         """
         Marks the status column of a task as 'Completed.'
@@ -154,6 +143,11 @@ class TaskLists:
     """
     Curates functions relating to organizing tasks into lists
     """
+
+    @staticmethod
+    def print_list_not_table(query_results):
+        pass
+        # TODO work on a list/dict comprehension for string formatting a nice list
 
     @staticmethod
     def print_any_list_choice(query_results):
@@ -178,7 +172,6 @@ class TaskLists:
         # print("-"*100)
 
     @staticmethod
-    # @app.command(short_help="Adds a task to the task database.")
     def database_report():
         query = tm.Tasks.select().dicts()
         formatted_list = []
@@ -203,7 +196,6 @@ class TaskLists:
 
     # List all tasks sorted by task number
     @staticmethod
-    # @app.command(short_help="Adds a task to the task database.")
     def task_list_id_sort(choice):
         # filtered_options = ['In Progress', 'Complete']
         list_of_dicts = []
@@ -231,7 +223,6 @@ class TaskLists:
 
     # List all tasks sorted by priority
     @staticmethod
-    # @app.command(short_help="Adds a task to the task database.")
     def task_list_priority_sort():
         """
         Uses peewee to query SQLite database for all (non-deleted) tasks
@@ -247,7 +238,6 @@ class TaskLists:
 
     # List all open tasks sorted by due date
     @staticmethod
-    # @app.command(short_help="Adds a task to the task database.")
     def task_list_open_sort(choice):
         """
         Uses peewee to query SQLite database for all (non-deleted tasks, incomplete) and sorts them by date.
@@ -277,7 +267,6 @@ class TaskLists:
 
     # List all closed tasks between specified dates
     @staticmethod
-    # @app.command(short_help="Adds a task to the task database.")
     def task_list_completed_sort(date_1, date_2):
         """
         Uses peewee to query SQLite database for completed tasks that fall within a given date range
@@ -297,7 +286,6 @@ class TaskLists:
 
     # List all overdue tasks
     @staticmethod
-    # @app.command(short_help="Adds a task to the task database.")
     def task_list_overdue_sort():
         """
         Uses peewee to query SQLite database for overdue tasks
@@ -310,7 +298,3 @@ class TaskLists:
             list_of_dicts.append(result)
         TaskLists.print_any_list_choice(list_of_dicts)
 
-
-if __name__ == '__main__':
-    tm.main_task_database()
-    # app()
