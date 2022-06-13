f5d0dda2ea5f4fbea3ce0731b4f23b748add5ea8
bburdel <94432814+bburdel@users.noreply.github.com>
2022-06-12 19:29:36 -0700

    Used code from class to get the web environment running. Code not sort the peewee documentation to do this so used sqlalchemy for now.

--- src/todolistapp/app.py
+++ src/todolistapp/app.py
@@ -3,10 +3,35 @@ Working with Flask, cURL, and JSON
 """
 
 from flask import Flask
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from flask import jsonify
+# import peewee as pw
 
-app = Flask(__name__)
+db_connect = create_engine("sqlite:///taskmodel.db")
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+class TasksNames(Resource):
+    def get(self):
+        # open connection to the database
+        conn = db_connect.connect()
+        query = conn.execute(
+            "select * from Tasks")
+        result = {"task name": [row[1] for row in query.cursor.fetchall()]}
+        conn.connect()
+        return jsonify(result)
+
+
+def main():
+    app = Flask(__name__)
+
+    api = Api(app)
+    api.add_resource(TasksNames, "/tasks")  # Route 1
+
+    app.run(port=5000)
+
+    db_connect.dispose()
+
+
+if __name__ == '__main__':
+    main()
