3c703e7a9cdd3aad51ed3de60bd9450213afd985
bburdel <94432814+bburdel@users.noreply.github.com>
2022-06-12 17:55:51 -0700

    Test flask app worked with the tutorial function.

---
+++ src/todolistapp/app.py
@@ -0,0 +1,12 @@
+"""
+Working with Flask, cURL, and JSON
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
--- src/todolistapp/flask_todo.py
+++
@@ -1,4 +0,0 @@
-"""
-Working with Flask, cURL, and JSON
-"""
-
